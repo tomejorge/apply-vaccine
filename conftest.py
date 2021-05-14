58de6fb8094c36a52cf93db9382339681ab1b361
tomejorge <tome.gaspar@gmail.com>
2021-05-14 18:17:13 +0200

    wip

--- conftest.py
+++ conftest.py
@@ -7,6 +7,9 @@ from selenium import webdriver
 @pytest.fixture(scope="class")
 def browser(request):
     options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--enable-automation')
     options.add_argument("--start-maximized")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--disable-extensions")
@@ -15,10 +18,10 @@ def browser(request):
     options.add_argument('--no-sandbox')
     options.add_argument('--ignore-certificate-errors')
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    driver = webdriver.Chrome(options=options, executable_path=ChromeDriverManager().install())
+    driver = webdriver.Chrome(options=options)
     request.cls.driver = driver
     driver.maximize_window()
 
     yield webdriver
-    # driver.close()
-    # driver.quit()
+    driver.close()
+    driver.quit()
